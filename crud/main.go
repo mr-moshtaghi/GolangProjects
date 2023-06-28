b6d04f6a22eea28db28b3e6d4315b81eb0c797e2
ahmad_ali_moshtaghi <moshtaghi@gmail.com>
2023-06-28 20:15:33 +0330

    create updateMovie and end project

--- crud/main.go
+++ crud/main.go
@@ -61,6 +61,24 @@ func createMovie(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(movie)
 }
 
+func updateMovie(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+
+	// The existing movie will be deleted and a new movie will be added
+
+	for index, item := range movies {
+		if item.ID == params["id"] {
+			movies = append(movies[:index], movies[index+1:]...)
+			var movie Movie
+			_ = json.NewDecoder(r.Body).Decode(&movie)
+			movie.ID = params["id"]
+			movies = append(movies, movie)
+			json.NewEncoder(w).Encode(movie)
+		}
+	}
+}
+
 func main() {
 	r := mux.NewRouter()
 
