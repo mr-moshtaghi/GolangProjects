b1e6c4d509600bfed59283c44a817688b3f09782
ahmad_ali_moshtaghi <moshtaghi@gmail.com>
2023-06-30 16:30:54 +0330

    create ValidateJWT

--- basic jwt/main.go
+++ basic jwt/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"github.com/dgrijalva/jwt-go"
 	"net/http"
 )
 
@@ -17,4 +18,32 @@ func GetJwt(w http.ResponseWriter, r *http.Request) {
 			fmt.Fprint(w, token)
 		}
 	}
+}
+
+func ValidateJWT(next func(w http.ResponseWriter, r *http.Request)) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+
+		if r.Header["Token"] != nil {
+			token, err := jwt.Parse(r.Header["Token"][0], func(t *jwt.Token) (interface{}, error) {
+				_, ok := t.Method.(*jwt.SigningMethodHMAC)
+				if !ok {
+					w.WriteHeader(http.StatusUnauthorized)
+					w.Write([]byte("not authorized"))
+				}
+				return SECRET, nil
+			})
+
+			if err != nil {
+				w.WriteHeader(http.StatusUnauthorized)
+				w.Write([]byte("not authorized: " + err.Error()))
+			}
+
+			if token.Valid {
+				next(w, r)
+			}
+		} else {
+			w.WriteHeader(http.StatusUnauthorized)
+			w.Write([]byte("not authorized"))
+		}
+	})
 }
